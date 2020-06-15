e590301993764845843d9aa826fdacaf3d247480
kbieniasz <kbieniasz@onet.eu>
2020-06-15 23:20:26 +0200

    Not good version

---
+++ src/pages/Details/Details.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+        import './Details.css';
+
+        const Details: React.FC = () => {
+
+        }
+
+        export default Details;
--- src/pages/progress/Progress.tsx
+++ src/pages/progress/Progress.tsx
@@ -1,62 +1,193 @@
-import { IonButton, IonModal, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonMenu, IonList, IonItem, IonButtons, IonMenuButton, IonTabs, IonTabBar, IonIcon, IonTabButton, IonBadge, IonLabel, IonItemGroup, IonItemDivider, IonInput, IonToggle, IonCheckbox, IonRange, IonText, IonItemSliding, IonItemOption, IonItemOptions, IonNote, IonAvatar, IonListHeader, IonChip, IonGrid, IonRow, IonCol, IonImg, IonAlert, } from '@ionic/react';
-import React, { useState } from 'react';
+
+
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+
+
+
+const Trainings: React.FC = () => {
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [30, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+
+
+    return (
+          <div>
+            <h2>My progress</h2>
+            <Bar
+              data={data}
+              width={1000}
+              height={500}
+              options={{
+                maintainAspectRatio: true
+              }}
+            />
+          </div>
+        );
+
+};
+
+export default Trainings;
+
+
+
+
+{/*
+import React from 'react'
+//
+import useChartConfig from 'hooks/useChartConfig'
+import { Chart } from 'react-charts'
+let sourceCode
+
+const Trainings: React.FC = () => {
+  const { data, randomizeData } = useChartConfig({
+    series: 8,
+    datums: 3,
+    dataType: 'ordinal'
+  })
+  const series = React.useMemo(
+    () => ({
+      type: 'bar'
+    }),
+    []
+  )
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'ordinal', position: 'bottom' },
+      { position: 'left', type: 'linear', stacked: false }
+    ],
+    []
+  )
+  return (
+    <>
+      <button onClick={randomizeData}>Randomize Data</button>
+      <br />
+      <br />
+        <Chart data={data} series={series} axes={axes} tooltip />
+
+      <br />
+    </>
+  )
+}
+export default Trainings;
+*/}
+
+{/*
+import React from 'react'
+import useChartConfig from 'hooks/useChartConfig'
+import Box from 'components/Box'
+import { Chart } from 'react-charts'
+import { IonContent} from '@ionic/react';
+
+let sourceCode
+
+
+
+const Progress: React.FC = () => {
+  const { data, randomizeData } = useChartConfig({
+    series: 10,
+    dataType: 'ordinal'
+  })
+  const series = React.useMemo(
+    () => ({
+      type: 'bar'
+    }),
+    []
+  )
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'ordinal', position: 'left' },
+      { position: 'bottom', type: 'linear', stacked: true }
+    ],
+    []
+  )
+  return (
+    <>
+      <button onClick={randomizeData}>Randomize Data</button>
+      <br />
+      <br />
+      <IonContent>
+        <Chart data={data} series={series} axes={axes} tooltip />
+      </IonContent>
+      <br />
+    </>
+  )
+}
+export default Progress;
+
+
+{/*
+import { IonButton, IonModal,  IonFooter, IonMenu, IonList, IonItem, IonMenuButton, IonTabs, IonTabBar, IonTabButton, IonBadge, IonLabel, IonItemGroup, IonItemDivider, IonInput, IonToggle, IonCheckbox, IonRange, IonText, IonItemSliding, IonItemOption, IonItemOptions, IonNote, IonAvatar, IonListHeader, IonChip, IonGrid,  IonImg, IonAlert, } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './Progress.css';
 import { calendar, personCircle, map, informationCircle, personCircleOutline, calendarOutline, personOutline, locationOutline, locationSharp, calendarSharp, personSharp } from 'ionicons/icons';
 import { IonReactRouter } from '@ionic/react-router';
-import { Chart } from 'chart.js';
+
+import { Chart } from 'react-charts'
+
 
 import { Component, ViewChild } from '@angular/core';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
-export class HomePage {
-  @ViewChild('barChart') barChart;
+import { IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonRow,
+  IonCol,
+  IonButtons,
+  IonIcon
+ } from '@ionic/react';
 
-  bars: any;
-  colorArray: any;
-  constructor() { }
-
-  ionViewDidEnter() {
-    this.createBarChart();
-  }
-
-  createBarChart() {
-    this.bars = new Chart(this.barChart.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Viewers in millions',
-          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
-          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-}
-
-
+import { Bar } from 'react-chartjs-2';
 
 
 const Progress: React.FC = () => {
 
   const [showJoin, setShowJoin] = useState(false);
+
+    const data = React.useMemo(
+        () => [
+          {
+            label: 'Series 1',
+            data: [
+              [0, 1],
+              [1, 2],
+              [2, 4],
+              [3, 2],
+              [4, 7],
+            ],
+          },
+          {
+            label: 'Series 2',
+            data: [
+              [0, 3],
+              [1, 1],
+              [2, 5],
+              [3, 6],
+              [4, 4],
+            ],
+          },
+        ],
+        []
+      )
 
   return (
     <IonPage>
@@ -70,8 +201,77 @@ const Progress: React.FC = () => {
       </IonHeader>
 
 
+        <IonContent>
+                <canvas id="myChart" width="400" height="400"></canvas>
+                <script>
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+                </script>
 
 
+
+                <IonCard color="light">
+                  <IonCardHeader>
+                    <IonCardTitle>Latest Situation per day</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <IonGrid>
+                      <IonRow>
+                        <IonCol size="12" size-sm>
+                          Confirmed Cases: <strong>12</strong>
+                        </IonCol>
+                        <IonCol size="12" size-sm>
+                          <Bar
+                            data={data}
+                            width={100}
+                            height={100}
+                            options={{
+                              maintainAspectRatio: true
+                            }}
+                          />
+                        </IonCol>
+                      </IonRow>
+
+                    </IonGrid>
+                  </IonCardContent>
+                </IonCard>
+              </IonContent>
+        {/*
       <IonContent>
 
           <ionListHeader color="light">Vertical Bar Chart</ionListHeader>
@@ -103,8 +303,10 @@ const Progress: React.FC = () => {
           </IonItem>
         </IonList>
       </IonContent>
+      A JSX comment
     </IonPage>
   );
 };
 
 export default Progress;
+*/}
