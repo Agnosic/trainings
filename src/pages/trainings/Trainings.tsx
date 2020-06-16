84efa51b3d8e627ec27801a76f9d167fcac2599f
Jakub <imaster0x@gmail.com>
2020-06-16 17:59:26 +0200

    Floating button fix

--- src/pages/trainings/Trainings.tsx
+++ src/pages/trainings/Trainings.tsx
@@ -4,48 +4,55 @@ import ExploreContainer from '../../components/ExploreContainer';
 import './Training.css';
 import { calendar, personCircle, map, informationCircle, personCircleOutline, calendarOutline, personOutline, locationOutline, locationSharp, calendarSharp, personSharp, notificationsSharp } from 'ionicons/icons';
 import { IonReactRouter } from '@ionic/react-router';
+import { addOutline } from 'ionicons/icons';
 
 const Trainings: React.FC = () => {
 
   const [showJoin, setShowJoin] = useState(false);
 
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar class='header'>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle color="dark">Trainings</IonTitle>
-          <IonButton color="tertiary" routerLink={"/notifications"} fill="clear" slot="end">
-            <IonIcon slot="icon-only" icon={notificationsSharp} />
-          </IonButton>
-        </IonToolbar>
-      </IonHeader>
+		<IonPage>
+			<IonHeader>
+				<IonToolbar class="header">
+					<IonButtons slot="start">
+						<IonMenuButton />
+					</IonButtons>
+					<IonTitle color="dark">Trainings</IonTitle>
+					<IonButton
+						color="tertiary"
+						routerLink={"/notifications"}
+						fill="clear"
+						slot="end"
+					>
+						<IonIcon slot="icon-only" icon={notificationsSharp} />
+					</IonButton>
+				</IonToolbar>
+			</IonHeader>
 
-
-      <IonContent>
-        <IonList lines="none">
-          <IonItem routerLink="/trainings/1">
-            <IonLabel>Karate sparring</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Judo sparring</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Basketball begginers</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Golf extreme challenges</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Chess</IonLabel>
-          </IonItem>
-        </IonList>
-        <IonFabButton href="newtraining">Add</IonFabButton>
-      </IonContent>
-    </IonPage>
-  );
+			<IonContent>
+				<IonList lines="none">
+					<IonItem routerLink="/trainings/1">
+						<IonLabel>Karate sparring</IonLabel>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Judo sparring</IonLabel>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Basketball begginers</IonLabel>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Golf extreme challenges</IonLabel>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Chess</IonLabel>
+					</IonItem>
+				</IonList>
+				<IonFabButton className="floating-button" routerLink="newtraining">
+					<IonIcon icon={addOutline} />
+				</IonFabButton>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Trainings;
